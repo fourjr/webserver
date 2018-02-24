903da74eba8f6ad6293cd2c44e45e7067259a823
fourjr <yeejiarong2004@gmail.com>
2018-02-24 22:37:15 +0800

    add dynamic /contants generating, add hourly /constants update for sanic response:

--- webserver.py
+++ webserver.py
@@ -1,17 +1,52 @@
 import aiohttp
 import json
 import os
+import asyncio
+from bs4 import BeautifulSoup
 from sanic import Sanic, response
 from motor.motor_asyncio import AsyncIOMotorClient
 
 app = Sanic(__name__)
 app.mongo = AsyncIOMotorClient(os.environ.get('mongo'))
 app.session = None
+app.cr_constants = None
+
+async def update_constants():
+    '''Updates constants for /cr/constants'''
+    while not app.session.closed:
+        output = {'info':'This data is updated hourly.', 'source':'https://cr-api.github.io/cr-api-data'}
+        async with app.session.get('https://www.github.com/cr-api/cr-api-data/tree/master/json') as resp:
+            soup = BeautifulSoup(await resp.text(), 'html.parser')
+
+        urls = [
+            i.find('td', attrs={'class':'content'})\
+                .find('span')\
+                .find('a')['title']
+            for i in
+            soup.find('div', attrs={'class':'application-main '})\
+                .find('div', attrs={'class':'', 'itemtype':'http://schema.org/SoftwareSourceCode'})\
+                .find('div')\
+                .find('div', attrs={'class':'container new-discussion-timeline experiment-repo-nav '})\
+                .find('div', attrs={'class':'repository-content '})\
+                .find('div', attrs={'class':'file-wrap'})\
+                .find('table', attrs={'class':'files js-navigation-container js-active-navigation-container'})\
+                .find('tbody')\
+                .find_all('tr', attrs={'class':'js-navigation-item'})
+        ]
+
+        for i in urls:
+            async with app.session.get('https://cr-api.github.io/cr-api-data/json/' + i) as z:
+                output[i.replace('.json', '')] = await z.json()
+
+        app.constants = output
+
+        await asyncio.sleep(3600)
 
 @app.listener('before_server_start')
 async def create_session(app, loop):
     '''Creates an aiohttp.ClientSession upon app connect'''
     app.session = aiohttp.ClientSession(loop=loop)
+    app.loop.create_task(update_constants())
 
 @app.listener('after_server_stop')
 async def close_session(app, loop):
@@ -21,35 +56,12 @@ async def close_session(app, loop):
 @app.route('/')
 async def startup(request):
     '''A / page, much like a ping'''
-    return response.json({'hello':'hell'})
+    return response.json({'hello':'world'})
 
 @app.route('/cr/constants')
 async def cr_constants(request):
     '''Retrieve constants from cr-api-data'''
-    output = {'source':'https://cr-api.github.io/cr-api-data'}
-    urls = [
-        'alliance_badges',
-        'arenas',
-        'cards',
-        'cards_stats',
-        'challenges',
-        'chest_order',
-        'clan_chest',
-        'game_modes',
-        'rarities',
-        'regions',
-        'tournaments',
-        'treasure_chests'
-    ]
-
-    for i in urls:
-        async with app.session.get('https://cr-api.github.io/cr-api-data/json/' + i + '.json') as z:
-            try:
-                output[i] = await z.json()
-            except aiohttp.client_exceptions.ContentTypeError:
-                pass
-
-    return response.json(output)
+    return response.json(app.constants)
 
 @app.route('/debug', methods=['GET', 'PUT', 'POST', 'GET', 'DELETE', 'PATCH'])
 async def debug(request):
